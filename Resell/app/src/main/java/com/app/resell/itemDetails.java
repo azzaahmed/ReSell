db3fc40386921d1afa00c29a3b766ec6b98766cc
Azza Ahmed <aazza_ahmed@hotmail.com>
2017-05-04 01:52:28 +0200

    add share fab

--- Resell/app/src/main/java/com/app/resell/itemDetails.java
+++ Resell/app/src/main/java/com/app/resell/itemDetails.java
@@ -1,8 +1,10 @@
 package com.app.resell;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,15 +17,18 @@ public class itemDetails extends AppCompatActivity {
         setContentView(R.layout.activity_item_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        final Activity activity=this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(activity)
+                        .setType("text/plain")
+                        .setText("Some sample text")
+                        .getIntent(), getString(R.string.action_share)));
             }
         });
+
     }
 
 }
