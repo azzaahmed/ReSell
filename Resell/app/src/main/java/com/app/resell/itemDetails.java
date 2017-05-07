c766ea648d0b5d38ed4c8a573b924ada0359ac09
Azza Ahmed <aazza_ahmed@hotmail.com>
2017-05-07 19:45:58 +0200

    edit itemdetails layout+ remove it toolbar+ make my items toolbar react to scroll

--- Resell/app/src/main/java/com/app/resell/itemDetails.java
+++ Resell/app/src/main/java/com/app/resell/itemDetails.java
@@ -24,8 +24,7 @@ public class itemDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         final Activity activity=this;
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
