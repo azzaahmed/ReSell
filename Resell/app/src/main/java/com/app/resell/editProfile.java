684bc94d1747d08a14633b5fe811b43413a99b8e
Azza Ahmed <aazza_ahmed@hotmail.com>
2017-05-08 11:15:27 +0200

    remove unwanted fab in sign up

--- Resell/app/src/main/java/com/app/resell/editProfile.java
+++ Resell/app/src/main/java/com/app/resell/editProfile.java
@@ -20,8 +20,6 @@ import android.widget.Toast;
 import com.app.resell.Data.FireBaseCalls;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.mukesh.countrypicker.fragments.CountryPicker;
 import com.mukesh.countrypicker.interfaces.CountryPickerListener;
 import com.mukesh.countrypicker.models.Country;
@@ -30,19 +28,18 @@ import com.mukesh.countrypicker.models.Country;
 public class editProfile extends AppCompatActivity{
 
 
-    private EditText Name;
+
     private EditText age;
     private EditText mobile;
     private Spinner gender;
 
     String mage;
-    String mname;
+
     String mgender;
     String mmobile,oldurl;
     Account account;
     private FirebaseAuth firebaseAuth;
-    //defining a database reference
-    private DatabaseReference databaseReference;
+
     private FireBaseCalls FireBaseCalls;
     FirebaseUser user;
 
@@ -63,14 +60,13 @@ public class editProfile extends AppCompatActivity{
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         firebaseAuth = FirebaseAuth.getInstance();
-        //getting the database reference
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         Intent intent = getIntent();
         account = (Account)intent.getSerializableExtra("accountinfo");
 
         age = (EditText) findViewById(R.id.age);
         gender = (Spinner) findViewById(R.id.gender);
-        Name = (EditText) findViewById(R.id.name);
+
         mobile = (EditText) findViewById(R.id.mobile);
 
         FireBaseCalls= new FireBaseCalls();
@@ -85,14 +81,14 @@ public class editProfile extends AppCompatActivity{
     private void edit(){
         user = firebaseAuth.getCurrentUser();
         mage=age.getText().toString().trim();
-        mname= Name.getText().toString().trim();
+
         mmobile=mobile.getText().toString().trim();
         mgender=gender.getSelectedItem().toString();
 
 
         if(account!=null) {
             if (mage.isEmpty()) mage = account.getAge();
-            if (mname.isEmpty()) mname = account.getName();
+
             if (mmobile.isEmpty()) mmobile = account.getMobile();
             // msmoker = smoker.getSelectedItem().toString();////////////////////////////////
             // get selected radio button from radioGroup
@@ -104,11 +100,6 @@ public class editProfile extends AppCompatActivity{
         }
 
         else{
-            if(TextUtils.isEmpty(mname)&&user.getDisplayName()==null){
-                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show();
-                return;
-
-            } else if(TextUtils.isEmpty(mname)&&user.getDisplayName()!=null) mname=user.getDisplayName();
 
             if(TextUtils.isEmpty(mgender)){
                 Toast.makeText(this,"Please enter your gender",Toast.LENGTH_LONG).show();
@@ -143,7 +134,7 @@ public class editProfile extends AppCompatActivity{
         }
 
         if(isOnline())
-        FireBaseCalls.AddFireBaseExtraInfo(mname, mage, mmobile, mgender, user.getEmail(), user.getPhotoUrl() + "", this,country);
+        FireBaseCalls.AddFireBaseExtraInfo(user.getDisplayName(), mage, mmobile, mgender, user.getEmail(), user.getPhotoUrl() + "", this,country);
         else  Toast.makeText(this, "no internet connection", Toast.LENGTH_LONG).show();
 
     }
